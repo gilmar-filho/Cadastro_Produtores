9953ce7cc1f8339a554ba1331053cbd92ce52c07
gilmar-filho <gfilho416@gmail.com>
2025-04-23 12:12:16 -0300

    Mudança no forms.py

--- cadastro/forms.py
+++ cadastro/forms.py
@@ -14,6 +14,7 @@ class PessoaForm(forms.ModelForm):
             'endereco': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
         }
 '''
+'''
 from django import forms
 from .models import Pessoa
 
@@ -51,3 +52,66 @@ class PessoaForm(forms.ModelForm):
 
         if not all([nome, cpf, telefone1, endereco]):
             raise forms.ValidationError("Os campos Nome, CPF, Telefone 1 e Endereço são obrigatórios.")
+'''
+from django import forms
+from .models import Pessoa
+
+class PessoaForm(forms.ModelForm):
+    class Meta:
+        model = Pessoa
+        fields = '__all__'
+        widgets = {
+            'nome': forms.TextInput(attrs={
+                'class': 'form-control border',
+                'placeholder': 'Digite o nome...'
+            }),
+            'cpf': forms.TextInput(attrs={
+                'class': 'form-control border',
+                'placeholder': 'Digite o CPF...'
+            }),
+            'endereco': forms.TextInput(attrs={
+                'class': 'form-control border',
+                'placeholder': 'Digite o endereço...'
+            }),
+            'numero_casa': forms.TextInput(attrs={
+                'class': 'form-control border',
+                'placeholder': 'Número da casa'
+            }),
+            'zona_rural': forms.CheckboxInput(attrs={
+                'class': 'form-check-input'
+            }),
+            'telefone1': forms.TextInput(attrs={
+                'class': 'form-control border',
+                'placeholder': 'Telefone principal'
+            }),
+            'telefone2': forms.TextInput(attrs={
+                'class': 'form-control border',
+                'placeholder': 'Segundo telefone (opcional)'
+            }),
+            'cartao_produtor': forms.TextInput(attrs={
+                'class': 'form-control border',
+                'placeholder': 'Cartão do Produtor'
+            }),
+            'checkbox_1': forms.CheckboxInput(attrs={
+                'class': 'form-check-input'
+            }),
+            'checkbox_2': forms.CheckboxInput(attrs={
+                'class': 'form-check-input'
+            }),
+            'checkbox_3': forms.CheckboxInput(attrs={
+                'class': 'form-check-input'
+            }),
+            'checkbox_4': forms.CheckboxInput(attrs={
+                'class': 'form-check-input'
+            }),
+            'relatorio': forms.Textarea(attrs={
+                'class': 'form-control border',
+                'placeholder': 'Escreva o relatório ou observações...'
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(PessoaForm, self).__init__(*args, **kwargs)
+        obrigatorios = ['nome', 'cpf', 'endereco', 'telefone1']
+        for field in obrigatorios:
+            self.fields[field].required = True
